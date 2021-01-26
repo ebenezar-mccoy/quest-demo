c8da40c2698fae0742a82f7911bbe8fee30e41f8
Mariusz Makowski <mariusz.makowski@regalcinemas.com>
2021-01-26 09:08:47 -0500

    prettify task_definition section

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -20,42 +20,26 @@ resource "aws_ecs_service" "quest-demo-ecs" {
     assign_public_ip = true
     security_groups  = var.vpc-security-group-ids
   }
-
-  depends_on = [aws_lb_listener.listener, aws_iam_role_policy_attachment.ecsTaskExecutionRole_policy]
 }
 
 data "http" "secret" {
   url = "http://${var.node-server-public-ip}:3000"
 }
 
+data "template_file" "quest-demo-task-definition" {
+  template = file("${path.module}/task-definition/quest-demo-task-definition.json")
+  vars = {
+    task_name   = "${var.name}-task"
+    secret_word = regex("find..(.*).the ", data.http.secret.body)[0]
+  }
+}
+
 resource "aws_ecs_task_definition" "quest-demo-task" {
-  family                   = "${var.name}-task" # Naming our first task
-  container_definitions    = <<DEFINITION
-  [
-    {
-      "name": "${var.name}-task",
-      "image": "ebenezar/quest-demo",
-      "essential": true,
-      "portMappings": [
-        {
-          "containerPort": 3000,
-          "hostPort": 3000
-        }
-      ],
-      "environment": [
-        {
-          "name": "SECRET_WORD",
-          "value": "${regex("find..(.*).the ", data.http.secret.body)[0]}"
-        }
-      ],
-      "memory": 512,
-      "cpu": 256
-    }
-  ]
-  DEFINITION
-  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  family                   = "${var.name}-task"
+  container_definitions    = data.template_file.quest-demo-task-definition.rendered
+  requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
-  memory                   = 512         # Specifying the memory our container requires
-  cpu                      = 256         # Specifying the CPU our container requires
+  memory                   = 512
+  cpu                      = 256
   execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
 }
