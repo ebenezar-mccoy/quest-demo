9f2574bd5998d6adcf86436a4d75f704d0195177
Mariusz Makowski <mariusz.makowski@regalcinemas.com>
2021-01-26 09:09:51 -0500

    variables cleanup

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
-variable "aws-access-key" { type = string }
-variable "aws-secret-key" { type = string }
-variable "ssh-public-key" { type = string }
+variable "ssh-public-key" {
+  type = string
+}
 
 variable "aws-region" {
   type    = string
